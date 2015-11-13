e90cfd0211bd8f3311bd308a023447711aaf0731
Ismail SEZEN <sezenismail@gmail.com>
2015-11-13 16:28:14 +0200

    Fixed errors in Nicker c++ codes
    * C++ codes were unstable and we’ve been got “segmentation fault
    error”. All of them fixed and it’s working like a charm at the moment.
    
    * Now, It is required to arrange cpp code to get out nice outputs
    compatible with gunplot or different data manipulation applications.
    
    * Unecessary extension definitions were added to .gitignore to prevent
    committing to repository like XCODE project files etc.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <iomanip> // needed to use manipulators with parameters (precision, width)
+// #include <cstring>
 #include "numdev.h"
 using namespace std;
 
 numdev nd;
+
+#define VAL(f,i,j) ( *(f+((i)*m+(j))) )
+#define SWAP(x,y) int xy;xy=x;x=y;y=xy;
+#define loop(i, n, m) for(int i = n; i < m; ++i)
+#define loop2(i, imin, imax, j, jmin, jmax) loop(i, imin, imax) loop(j, jmin, jmax)
 
 void test() {
 //    int arr[3][4] ={
@@ -18,11 +25,9 @@ void test() {
 }
 
 void test_lap5p_o2() {
-    double arr[3][3] ={
-        {1,2,3},
-        {4,6,6},
-        {7,8,9}
-    };
+//    double arr[3][3] ={{1,2,3},
+//                       {4,6,6},
+//                       {7,8,9}};
     //double res_lap = nd.lap5p_o2(&arr[1][1], 3, 1);
 
     //cout << (res_lap==-4?"OK":"ERROR") << "\n";
@@ -79,183 +84,147 @@ void test_jac9p_o2() {
     //cout << res_jac << "\n";
 }
 
-void nicker() {
-    int l = 39, n = l, m = 61;
-    int n1 = n-1, m1 = m-1, n2 = n-2, m2 = m-2;
-    double dt = 3;
-    int loopt = 40;
-    double gnu = 0.5;
-    double dx = 10, e = dx;
-    double tneut = 300;
-    double g = 9.81;
-    int nold = 0;
-    int nnew = 1;
-    double alpha = 1.89;
-
-    double psi[n][m], q[n][m], arak[n][m], difu[n][m], u[n][m], w[n][m], tmap[n][m], wrk[n][m];
-    double eta[n][m][2], t[n][m][2];
-
-    for(int i=0; i<=n1;i++){
-        for(int j=0; j<=m1; j++){
-            psi[i][j] = 0;
-            eta[i][j][nold] = 0;
-            t[i][j][nold] = 0;
-            q[i][j] = 0;
-            u[i][j] = 0;
-            w[i][j] = 0;
-        }
-    }
-
-    for(int i=1;i<=17;i++)
-        for(int j=11;j<=31;j++)
-            t[i-1][j-1][nold] = 0.5*cos(M_PI*(i-1)/32)*pow((cos(M_PI*((j-1)-10)/40)),2);
-
-    for(int i = 1;i<18;i++) t[i-1][9][nold] = t[i-1][10][nold];
-
-    // The non-adiabatic heating (deg/sec) is defined as:
-    for(int i=1;i<=17;i++)
-        for(int j=9;j<=13;j++)
-            q[i-1][j-1] = 4.e-03*cos(M_PI*(i-1)/32)*pow((cos(M_PI*((j-1)-10)/4)),2);
-
-    int time = 0;
-
-    for(int i=0;i<l;i++) for(int j=0;j<m;j++) wrk[i][j] = t[i][j][nold];
-
-    for(int i=0;i<16;i++) for(int j=9;j<30;j++) t[i][j][nold]=t[i][j][nold]/tneut;
-
-    int loop=0;
-    do{
-        loop++;
-        if(loop!=1){
-            int nsave=nold;
-            nold=nnew;
-            nnew=nsave;
-        }
-
-    // Define the heating as a function of time
-        for(int j=9;j<=13;j++){
-            for(int i=1;i<=17;i++){
-                double qq=4.e-03*cos(M_PI*(i-1)/32)*pow(cos(M_PI*((j-1)-10)/4),2);
-                q[i-1][j-1]=2*qq/M_PI;
-                if(loop>100 && loop<=200) q[i-1][j-1]=-q[i-1][j-1];
-            }
-        }
-
-        nd.jac9p_o2(&arak[0][0], &psi[0][0], &eta[0][0][nold],l,m,dx);
-        nd.lap5p_o2(&difu[0][0],&eta[0][0][nold],n,m,dx);
-
-//        if(loop==2){
-//        cout << "loop=" << loop << endl;
-//        //cout << time << endl;
-//        for(int i=0;i<n;i++) {
-//            cout << i << "/";
-//            for(int j=0;j<m;j++) {
-//                double v = arak[i][j];
-//                cout << v << " ";
-//            }
-//            cout << endl;
-//        }
-//        cout << endl;
-//        exit(1);
-//        }
-
-        for(int i=1;i<n1;i++)
-            for(int j=1;j<m1;j++)
-                eta[i][j][nnew]=eta[i][j][nold]+dt*arak[i][j]+dt*gnu*difu[i][j]-dt*g*(t[i+1][j][nold]-t[i-1][j][nold])/(2*dx);
-
-        for(int i=0;i<n;i++) eta[i][0][nnew]=eta[i][m-1][nnew]=0;
-        for(int j=0;j<m;j++) eta[0][j][nnew]=eta[n-1][j][nnew]=0;
-
-    // Calculate initial estimate of phi from heat transfer equation
-
-        nd.jac9p_o2(&arak[0][0], &psi[0][0], &t[0][0][nold],l,m,dx);
-        nd.lap5p_o2(&difu[0][0],&t[0][0][nold],n,m,dx);
-
-        for(int i=1;i<n;i++)
-            for(int j=1;j<m;j++)
-                t[i][j][nnew]=t[i][j][nold]+dt*arak[i][j]+dt*q[i][j]/tneut + dt*gnu*difu[i][j];
-
-    // Relax eta to get psi.routine RELAX1 solves the poisson equation
-
-        nd.RELAX1(&psi[0][0],&eta[0][0][nnew],n,m,alpha);
-
-//        if(loop==1){
-//            cout << "loop=" << loop << endl;
-//            //cout << time << endl;
-//            for(int j=0;j<m;j++) {
-//                cout << j << "/";
-//                for(int i=0;i<n;i++) {
-//                    double v = psi[i][j];
-//                    cout << v << " ";
-//                }
-//                cout << endl;
-//            }
-//            cout << endl;
-//            exit(1);
-//        }
-
-    // The horizontal velocity and vertical velocity are defined from the stream function
-
-        for(int i=0;i<n;i++) for(int j=0;j<m1;j++) u[i][j]=(psi[i][j+1]-psi[i][j])/dx;
-        for(int j=0;j<m;j++) for(int i=0;i<n1;i++) w[i][j]=-(psi[i+1][j]-psi[i][j])/dx;
-
-    // Calculate the final (corrector) estimate of eta and phi for this time step
-    // from the predicted vorticity and tempature fields
-
-        nd.jac9p_o2(&arak[0][0], &psi[0][0], &eta[0][0][nnew],l,m,dx);
-        nd.lap5p_o2(&difu[0][0],&eta[0][0][nnew],n,m,dx);
-
-        for(int i=1;i<n1;i++)
-            for(int j=1;j<m1;j++)
-                eta[i][j][nnew]=eta[i][j][nold]+dt*arak[i][j]+dt*gnu*difu[i][j]-dt*g*(t[i+1][j][nnew]-t[i-1][j][nnew])/(2*dx);
-
-    // Relax final estimate of eta to get final psi and consequently u and w fields.
-
-        nd.RELAX1(&psi[0][0],&eta[0][0][nnew],n,m,alpha);
-
-        for(int i=0;i<n;i++) for(int j=0;j<m1;j++) u[i][j]=(psi[i][j+1]-psi[i][j])/dx;
-        for(int j=0;j<m;j++) for(int i=0;i<n1;i++) w[i][j]=-(psi[i+1][j]-psi[i][j])/dx;
-
-    // Final estimate of phi for this time step.
-
-        nd.jac9p_o2(&arak[0][0], &psi[0][0], &t[0][0][nnew],l,m,dx);
-        nd.lap5p_o2(&difu[0][0], &t[0][0][nnew],n,m,dx);
-
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                t[i][j][nnew]=t[i][j][nold]+dt*arak[i][j]+dt*q[i][j]/tneut + dt*gnu*difu[i][j];
-
-        time+=dt;
-        cout << time << endl;
-        if(loop % loopt !=0) continue;
-        time=3*loop;
-
-        for(int i=0;i<n;i++) for(int j=0;j<m;j++) tmap[i][j]=t[i][j][nnew]*tneut;
-        for(int j=0;j<m;j++) for(int i=0;i<l;i++) wrk[i][j]=tmap[i][j];
-
-        cout << time << endl;
-        for(int j=0;j<m;j++) {
-            cout << j << "/";
-            for(int i=0;i<n;i++) cout << wrk[i][j] << " ";
-            cout << endl;
+void print_arr(double* a, int l, int m, const char* arr_name) {
+    
+    int wdth = 14;
+    cout << arr_name << "[i,j]" << endl;
+    cout << setprecision(6);
+    
+    cout << "    ";
+    for(int i=0;i<m;++i) cout << setw(wdth) << i;
+    cout << endl << "----";
+    for(int i=0;i<m;++i) cout << setw(wdth) << "--------------";
+    cout << endl;
+    for(int i=0;i<l;++i) {
+        cout << setw(3) << i << "|";
+        for(int j=0;j<m;++j){
+            double v = VAL(a,i,j);
+            if(v!=0)
+                cout << setw(wdth) << scientific << VAL(a,i,j);
+            else
+                cout << setw(wdth) << "0";
         }
         cout << endl;
-        exit(1);
-        for(int j=0;j<m;j++) w[n][j]=w[n1][j];
-        for(int i=0;i<n;i++) u[i][m]=u[i][m1];
+    }
+    cout << endl;
+}
 
+void nicker() {
+    int l = 39, n = l, m = 61;
+    int n1 = n-1, m1 = m-1;
+    double dt    = 3;
+    int loopt    = 40;
+    double gnu   = 0.5;
+    double dx    = 10;
+    double tneut = 300;
+    double g     = 9.81;
+    int nold     = 0;
+    int nnew     = 1;
+    double alpha = 1.89;
 
+    // Array Definitions
+    double psi[n][m], q[n][m], arak[n][m], difu[n][m], u[n][m], w[n][m], tmap[n][m], wrk[n][m];
+    double eta[2][n][m], t[2][n][m];
+    
+    // Initialize arrays
+    loop2(i, 0, n, j, 0, m) {
+        psi[i][j]       = 0;
+        eta[nold][i][j] = 0;
+        eta[nnew][i][j] = 0;
+        t[nold][i][j]   = 0;
+        t[nnew][i][j]   = 0;
+        q[i][j]         = 0;
+        u[i][j]         = 0;
+        w[i][j]         = 0;
+        arak[i][j]      = 0;
+        difu[i][j]      = 0;
+        tmap[i][j]      = 0;
+        wrk[i][j]       = 0;
+    }
+    
 
+    loop2(i, 1, 18, j, 11, 32) t[nold][i-1][j-1] = 0.5 * cos(M_PI*(i-1)/32) * pow(cos(M_PI*((j-1)-10)/40), 2);
+    loop(i, 1, 18)               t[nold][i-1][9] = t[nold][i-1][10];
+    loop2(i, 0, 16, j, 9, 30)      t[nold][i][j] = t[nold][i][j]/tneut;
+    loop2(i, 0, l, j, 0, m)            wrk[i][j] = t[nold][i][j];
+    
+    // The non-adiabatic heating (deg/sec) is defined as:
+    loop2(i, 1, 18, j, 9, 14) q[i-1][j-1] = 4e-03 * cos(M_PI*(i-1)/32) * pow(cos(M_PI*((j-1)-10)/4), 2);
+    
+    int time = 0;
+    int loop = 0;
+    do {
+        loop++;
+        if(loop != 1) {SWAP(nold, nnew);}
 
+        // Define the heating as a function of time
+        loop2(i, 1, 18, j, 9, 14) {
+            double          qq = 4.e-03 * cos(M_PI*(i-1)/32) * pow(cos(M_PI*((j-1)-10)/4), 2);
+                   q[i-1][j-1] = ((loop>100 && loop<=200)?(-2):(2))*qq/M_PI;
+        }
 
+        nd.jac9p_o2(&arak[0][0], &psi[0][0], &eta[nold][0][0],l, m, dx);
+        nd.lap5p_o2(&difu[0][0], &eta[nold][0][0], n, m, dx);
+        
+        loop2(i, 1, n1, j, 1, m1)
+            eta[nnew][i][j] = eta[nold][i][j] + dt * arak[i][j] + dt * gnu * difu[i][j] - dt * g * (t[nold][i+1][j]-t[nold][i-1][j])/(2*dx);
+
+        loop(i, 0, n) eta[nnew][i][0] = eta[nnew][i][m-1] = 0;
+        loop(j, 0, m) eta[nnew][0][j] = eta[nnew][n-1][j] = 0;
+        
+        // Calculate initial estimate of phi from heat transfer equation
+        nd.jac9p_o2(&arak[0][0], &psi[0][0], &t[nold][0][0], l, m, dx);
+        nd.lap5p_o2(&difu[0][0], &t[nold][0][0], n, m, dx);
+        
+        loop2(i, 0, n, j, 0, m) t[nnew][i][j] = t[nold][i][j] + dt * arak[i][j] + dt * q[i][j]/tneut + dt * gnu * difu[i][j];
+
+        // Relax eta to get psi.routine RELAX1 solves the poisson equation
+        nd.RELAX1(&psi[0][0], &eta[nnew][0][0], n, m, alpha);
+        
+        // The horizontal velocity and vertical velocity are defined from the stream function
+
+        loop2(i,0,n,j,0,m1) u[i][j] =  (psi[i][j+1] - psi[i][j])/dx;
+        loop2(j,0,m,i,0,n1) w[i][j] = -(psi[i+1][j] - psi[i][j])/dx;
+        
+        // Calculate the final (corrector) estimate of eta and phi for this time step
+        // from the predicted vorticity and tempature fields
+        nd.jac9p_o2(&arak[0][0], &psi[0][0], &eta[nnew][0][0], l, m, dx);
+        nd.lap5p_o2(&difu[0][0], &eta[nnew][0][0], n, m, dx);
+        
+        loop2(i,1,n1,j,1,m1)
+            eta[nnew][i][j] = eta[nold][i][j] + dt * arak[i][j] + dt * gnu * difu[i][j] - dt * g * (t[nnew][i+1][j]-t[nnew][i-1][j])/(2 * dx);
+        
+        // Relax final estimate of eta to get final psi and consequently u and w fields.
+        nd.RELAX1(&psi[0][0], &eta[nnew][0][0], n, m, alpha);
+        
+        loop2(i,0,n,j,0,m1) u[i][j] =  (psi[i][j+1]-psi[i][j])/dx;
+        loop2(j,0,m,i,0,n1) w[i][j] = -(psi[i+1][j]-psi[i][j])/dx;
+        
+        // Final estimate of phi for this time step.
+        nd.jac9p_o2(&arak[0][0], &psi[0][0], &t[nnew][0][0], l, m, dx);
+        nd.lap5p_o2(&difu[0][0], &t[nnew][0][0], n, m, dx);
+        
+        loop2(i,0,n,j,0,m) t[nnew][i][j] = t[nold][i][j] + dt * arak[i][j] + dt * q[i][j]/tneut + dt * gnu * difu[i][j];
+        
+        time += dt;
+        if(loop % loopt !=0) continue;
+//        time = 3*loop;
+        
+        loop2(i,0,n,j,0,m) tmap[i][j] = t[nnew][i][j] * tneut;
+        loop2(j,0,m,i,0,l)  wrk[i][j] = tmap[i][j];
+        
+        cout << time << endl;
+        print_arr(&wrk[0][0], l, m, "wrk"); exit(1);
+        
+        loop(j,0,m) w[n][j] = w[n1][j];
+        loop(i,0,n) u[i][m] = u[i][m1];
 
     }while(loop<200);
 
 }
 
 int main() {
-    cout << "Hello world!" << endl;
-    //test_jac9p_o2();
+//    test_jac9p_o2();
     nicker();
     //test();
     return 0;
--- cpp/numdev.cpp
+++ cpp/numdev.cpp
@@ -11,30 +11,28 @@ numdev::~numdev(){}
 void numdev::lap5p_o2(double* difu, double* t, int n, int m, double h) {
     int n1 = n-1, m1 = m-1;
     double da = h*h;
-    for(int i=1;i<n1;i++){
-        for(int j=1;j<m1;j++){
-            VAL(difu, i, j) = (VAL(t,i,j+1) + VAL(t, i, j-1) + VAL(t, i+1, j) + VAL(t, i-1, j) + 4*VAL(t,i,j))/da;
-        }
-    }
+    for(int i=1; i<n1; ++i)
+        for(int j=1; j<m1; ++j)
+            VAL(difu, i, j) = (VAL(t,i,j+1) + VAL(t, i, j-1) + VAL(t, i+1, j) + VAL(t, i-1, j) - 4*VAL(t,i,j))/da;
 
     // Side boundaries
-    for(int j=1;j<m1;j++) {
-        VAL(difu, 0, j) = (2*VAL(t,1,j)  + VAL(t, 0, j+1) + VAL(t, 0, j-1) - 4*VAL(t,0,j))/da;
+    for(int j=1; j<m1; ++j) {
+        VAL(difu, 0, j)   = (2*VAL(t,1,j)    + VAL(t, 0, j+1)   + VAL(t, 0, j-1)   - 4*VAL(t,0,j))/da;
         VAL(difu, n-1, j) = (2*VAL(t,n1-1,j) + VAL(t, n-1, j+1) + VAL(t, n-1, j-1) - 4*VAL(t,n-1,j))/da;
     }
 
     // Top and bottom boundaries
-    for(int i=1;i<n1;i++) {
-        VAL(difu, i, 0) = (2*VAL(t,i,1)  + VAL(t, i-1, 0) + VAL(t, i+1, 0) - 4*VAL(t,i,0))/da;
+    for(int i=1; i<n1; ++i) {
+        VAL(difu, i, 0)   = (2*VAL(t,i,1)    + VAL(t, i-1, 0)   + VAL(t, i+1, 0)   - 4*VAL(t,i,0))/da;
         VAL(difu, i, m-1) = (2*VAL(t,i,m1-1) + VAL(t, i-1, m-1) + VAL(t, i+1, m-1) - 4*VAL(t,i,m-1))/da;
     }
 
     double db = 2/da;
     // Four Corners
-    VAL(difu, 0, 0) = db*(VAL(t,0,1)  + VAL(t,1,0)  - 2*VAL(t,0,0));
-    VAL(difu, 0, m-1) = db*(VAL(t,0,m1-1) + VAL(t,1,m-1)  - 2*VAL(t,0,m-1));
+    VAL(difu, 0, 0)     = db*(VAL(t,0,1)      + VAL(t,1,0)      - 2*VAL(t,0,0));
+    VAL(difu, 0, m-1)   = db*(VAL(t,0,m1-1)   + VAL(t,1,m-1)    - 2*VAL(t,0,m-1));
     VAL(difu, n-1, m-1) = db*(VAL(t,n1-1,m-1) + VAL(t,n-1,m1-1) - 2*VAL(t,n-1,m-1));
-    VAL(difu, n-1, 0) = db*(VAL(t,n1-1,0) + VAL(t,n-1,1)  - 2*VAL(t,n-1,0));
+    VAL(difu, n-1, 0)   = db*(VAL(t,n1-1,0)   + VAL(t,n-1,1)    - 2*VAL(t,n-1,0));
 
 }
 
@@ -44,120 +42,91 @@ void numdev::jac9p_o2(double* a, double* p, double* z, int l, int m, double h) {
     j1(a, p, z, l, m, h);
 }
 
-void numdev::j1_first_col(double* a, double* p, double* z, int l, int m, double h){
-    int im1 = 0, ip1=0;
-    int l1=l-2;
-
-    for(int i=0;i<l;i++){
-       if((i-1)<1){
-            im1=l1;
-            ip1=1;
-       }else{
-        if ((i-l-1)<0){
-            im1=i-1;
-            ip1=i+1;
+void numdev::j1_first_col(double* a, double* p, double* z, int l, int m, double h) {
+    for(int i=0; i<l; ++i) {
+        int im1, ip1;
+        if(i==0 || i==(l-1)) {
+            im1 = l-2; ip1 = 1;
         }else{
-            im1=l1;
-            ip1=1;
+            im1 = i-1; ip1 = i+1;
         }
-       }
        VAL(a,i,0) = (VAL(p,i,0)   + VAL(p,ip1,0)  - VAL(p,i,1)    - VAL(p,ip1,1)) * (VAL(z,i,0)   + VAL(z,ip1,0)) -
                     (VAL(p,im1,0) + VAL(p,i,0)    - VAL(p,im1,1)  - VAL(p,i,1))   * (VAL(z,im1,0) + VAL(z,i,0)) +
                     (VAL(p,ip1,0) + VAL(p,ip1,1)  - VAL(p,im1,0)  - VAL(p,im1,1)) * (VAL(z,i,0)   + VAL(z,i,1)) +
                     (VAL(p,ip1,0) - VAL(p,i,1))   * (VAL(z,i,0)   + VAL(z,ip1,1)) +
                     (VAL(p,i,1)   - VAL(p,im1,0)) * (VAL(z,im1,1) + VAL(z,i,0));
-        VAL(a,i,0)= VAL(a,i,0)/(12*h*h);
+        VAL(a,i,0) /= (12*h*h);
     }
 }
 
-void numdev::j1_last_col(double* a, double* p, double* z, int l, int m, double h){
-    int im1 = 0, ip1=0;
-    int l1=l-2;
-
-    for(int i=0;i<l;i++){
-       if((i-1)<1){
-            im1=l1;
-            ip1=1;
-       }else{
-        if ((i-l-1)<0){
-            im1=i-1;
-            ip1=i+1;
+void numdev::j1_last_col(double* a, double* p, double* z, int l, int m, double h) {
+    for(int i=0; i<l; ++i) {
+        int im1, ip1;
+        if(i==0 || i==(l-1)) {
+            im1 = l-2; ip1 = 1;
         }else{
-            im1=l1;
-            ip1=1;
+            im1 = i-1; ip1 = i+1;
         }
-       }
        VAL(a,i,m-1) = (VAL(p,i,m-2)   + VAL(p,ip1,m-2)  - VAL(p,i,m-1)      - VAL(p,ip1,m-1)) * (VAL(z,i,m-1)   + VAL(z,ip1,m-1)) -
                       (VAL(p,im1,m-2) + VAL(p,i,m-2)    - VAL(p,im1,m-1)    - VAL(p,i,m-1))   * (VAL(z,im1,m-1) + VAL(z,i,m-1)) -
                       (VAL(p,ip1,m-2) + VAL(p,ip1,m-1)    - VAL(p,im1,m-2)  - VAL(p,im1,m-1)) * (VAL(z,i,m-2) + VAL(z,i,m-1)) -
                       (VAL(p,i,m-2)   - VAL(p,im1,m-1))   * (VAL(z,im1,m-2) + VAL(z,i,m-1)) -
                       (VAL(p,ip1,m-1)   - VAL(p,i,m-2))   * (VAL(z,i,m-1)     + VAL(z,ip1,m-2));
-        VAL(a,i,m-1)= VAL(a,i,m-1)/(12*h*h);
+        VAL(a,i,m-1) /= (12*h*h);
     }
 }
 
-void numdev::j1 (double* a, double* p, double* z, int l, int m, double h){
-   int im1 = 0, ip1=0;
-    int l1=l-2;
-
-
-    for(int j=1;j<(m-1);j++){
-        for(int i=0;i<l;i++){
-           if((i-1)<1){
-                im1=l1;
-                ip1=1;
-           }else{
-            if ((i-l-1)<0){
-                im1=i-1;
-                ip1=i+1;
-            }else{
-                im1=l1;
-                ip1=1;
-            }
-           }
-           VAL(a,i,j) = (VAL(p,i,j-1)   + VAL(p,ip1,j-1)  - VAL(p,i,j+1)    - VAL(p,ip1,j+1) ) * (VAL(z,ip1,j) - VAL(z,i,j)   ) +
-                        (VAL(p,im1,j-1) + VAL(p,i,j-1)    - VAL(p,im1,j+1)  - VAL(p,i,j+1)   ) * (VAL(z,i,j)   - VAL(z,im1,j) ) +
-                        (VAL(p,ip1,j)   + VAL(p,ip1,j+1)  - VAL(p,im1,j)    - VAL(p,im1,j+1) ) * (VAL(z,i,j+1) - VAL(z,i,j)   ) +
-                        (VAL(p,ip1,j-1) + VAL(p,ip1,j)    - VAL(p,im1,j-1)  - VAL(p,im1,j)   ) * (VAL(z,i,j)   - VAL(z,i,j-1) ) +
-                        (VAL(p,ip1,j)   - VAL(p,i,j+1))   * (VAL(z,ip1,j+1) - VAL(z,i,j)     ) +
-                        (VAL(p,i,j-1)   - VAL(p,im1,j))   * (VAL(z,i,j)     - VAL(z,im1,j-1) ) +
-                        (VAL(p,i,j+1)   - VAL(p,im1,j))   * (VAL(z,im1,j+1) - VAL(z,i,j)     ) +
-                        (VAL(p,ip1,j)   - VAL(p,i,j-1))   * (VAL(z,i,j)     - VAL(z,ip1,j-1) );
-            VAL(a,i,j) = VAL(a,i,j) / (12*h*h);
-            //VAL(a,i,j) /= (12*h*h);
+void numdev::j1(double* a, double* p, double* z, int l, int m, double h) {
+    for(int i=0; i<l; ++i) {
+        int im1, ip1;
+        if(i==0 || i==(l-1)) {
+            im1 = l-2; ip1 = 1;
+        }else{
+            im1 = i-1; ip1 = i+1;
+        }
+        for(int j=1; j<(m-1); j++) {
+            VAL(a,i,j) = (VAL(p,i,j-1)   + VAL(p,ip1,j-1)  - VAL(p,i,j+1)    - VAL(p,ip1,j+1) ) * (VAL(z,ip1,j) - VAL(z,i,j)   ) +
+                         (VAL(p,im1,j-1) + VAL(p,i,j-1)    - VAL(p,im1,j+1)  - VAL(p,i,j+1)   ) * (VAL(z,i,j)   - VAL(z,im1,j) ) +
+                         (VAL(p,ip1,j)   + VAL(p,ip1,j+1)  - VAL(p,im1,j)    - VAL(p,im1,j+1) ) * (VAL(z,i,j+1) - VAL(z,i,j)   ) +
+                         (VAL(p,ip1,j-1) + VAL(p,ip1,j)    - VAL(p,im1,j-1)  - VAL(p,im1,j)   ) * (VAL(z,i,j)   - VAL(z,i,j-1) ) +
+                         (VAL(p,ip1,j)   - VAL(p,i,j+1))   * (VAL(z,ip1,j+1) - VAL(z,i,j)     ) +
+                         (VAL(p,i,j-1)   - VAL(p,im1,j))   * (VAL(z,i,j)     - VAL(z,im1,j-1) ) +
+                         (VAL(p,i,j+1)   - VAL(p,im1,j))   * (VAL(z,im1,j+1) - VAL(z,i,j)     ) +
+                         (VAL(p,ip1,j)   - VAL(p,i,j-1))   * (VAL(z,i,j)     - VAL(z,ip1,j-1) );
+            VAL(a,i,j) /= (12*h*h);
         }
     }
 }
 
 void numdev::RELAX1(double* p, double* eta, int n, int m, double alpha){
-    double eps   = 1e-03;
+    double eps    = 0.001;
     double dx    = 10;
-    double nscan = 0;
+    int nscan    = 0;
     int n1       = n-1;
     int m1       = m-1;
-    double rmax, ps;
+    
     for(;;){
         nscan++;
-        rmax = 0;
+        double rmax = 0;
         int isave = 0, jsave =0;
         for(int i=1;i<n1;i++) {
             for(int j=1;j<m1;j++) {
                 double r1 = 0.25*(VAL(p,i+1,j) + VAL(p,i-1,j) + VAL(p,i,j+1) + VAL(p,i,j-1));
-                double r2 = -VAL(p,i,j) -  0.25*dx*dx*VAL(eta,i,j);
+                double r2 = -VAL(p,i,j) -  0.25 * dx * dx * VAL(eta,i,j);
                 double r  = r1 + r2;
                 if(rmax < abs(r)){
                     isave = i;
                     jsave = j;
                     rmax  = abs(r);
                 }
-                VAL(p,i,j) = VAL(p,i,j) + alpha*r;
+                VAL(p,i,j) = VAL(p,i,j) + alpha * r;
             }
         }
         if(nscan == 300){
             cout << "No Convergence achieved\n";
             exit(1);
         }
-        ps = abs(VAL(p,isave,jsave));
+        double ps = abs(VAL(p,isave,jsave));
         if(((rmax/ps-eps)<0)) break;
     }
     //while((rmax/ps-eps)>=0);
